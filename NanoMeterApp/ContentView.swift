3841401e830f447bd29dd43292814e9092feb742
woon <634226718@qq.com>
2025-11-05 17:47:47 +0800

    Initial Commit

---
+++ NanoMeterApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NanoMeterApp
+//
+//  Created by Woonsan on 2025/11/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
