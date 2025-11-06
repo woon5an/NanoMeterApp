9cfd863b6e54a723acc850885899affe64fd0060
袁咏仪的猫 <48851896+woon5an@users.noreply.github.com>
2025-11-06 20:59:38 +0800

    Refactor spot overlay into reusable shape

--- NanoMeterApp/ExposureNote.swift
+++ NanoMeterApp/ExposureNote.swift
@@ -1,2 +1,12 @@
 import Foundation
-struct ExposureNote:Identifiable,Codable{ var id:UUID=UUID(); var date:Date=Date(); var aperture:String="8"; var shutter:String="1/125"; var iso:String="400"; var ev:Double=0; var latitude:Double?; var longitude:Double? }
+
+struct ExposureNote: Identifiable, Codable, Equatable {
+    var id: UUID = UUID()
+    var date: Date = Date()
+    var aperture: String = "8"
+    var shutter: String = "1/125"
+    var iso: String = "400"
+    var ev: Double = 0
+    var latitude: Double?
+    var longitude: Double?
+}
